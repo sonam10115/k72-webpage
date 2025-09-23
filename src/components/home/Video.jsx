afa422fb90eb0221b9047d4c98d1b735c382108b
sonam10115 <kesharwanisonam60@gmail.com>
2025-09-23 21:23:35 +0530

    ok

---
+++ src/components/home/Video.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Video = () => {
+  return (
+    <div className="h-full w-full">
+      <video
+        className="h-full w-full object-cover"
+        autoPlay
+        loop
+        muted
+        src="..\..\..\public\video\video.mp4"
+      ></video>
+    </div>
+  );
+};
+
+export default Video;
